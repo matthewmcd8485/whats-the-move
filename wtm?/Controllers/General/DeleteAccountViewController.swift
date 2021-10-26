dec2c179cad49cc085a0ff114cd8547a9a6abcdb
Matt McDonnell <mattmcdonnell02@gmail.com>
2021-10-25 19:13:22 -0500

    Starting build 7

---
+++ wtm?/Controllers/General/DeleteAccountViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DeleteAccountViewController.swift
+//  wtm?
+//
+//  Created by Matthew McDonnell on 10/25/21.
+//
+
+import UIKit
+
+class DeleteAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
